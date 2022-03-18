5ffa7f419836f0089b834fe8a7e01952b33e7445
InEase <InEase28@gmail.com>
2022-03-18 12:58:30 +0800

    暂时更改树列表展示

--- web/src/pages/Detect/tree/constants.ts
+++ web/src/pages/Detect/tree/constants.ts
@@ -5,83 +5,61 @@ export const TREE_DATA = [
     value: 0,
     children: [
       {
-        label: '总办',
+        label: '本科室',
         value: '0-0',
       },
       {
-        label: '市场部',
+        label: '其他科室',
         value: '0-1',
         children: [
           {
-            label: '采购1组',
+            label: '患者A',
             value: '0-1-0',
           },
           {
-            label: '采购2组',
-            value: '采购2组',
+            label: '患者B',
+            value: '患者B',
           },
         ],
-      },
-      {
-        label: '技术部',
-        value: '0-2',
       },
     ],
   },
   {
-    label: '北京总部',
+    label: '3月16日患者',
     value: 1,
     children: [
       {
-        label: '总办',
+        label: '本科室',
         value: '1-0',
       },
       {
-        label: '市场部',
+        label: '其他科室',
         value: '1-1',
-        children: [
-          {
-            label: '采购1组',
-            value: '1-1-0',
-          },
-          {
-            label: '采购2组',
-            value: '1-1-1',
-          },
-        ],
       },
     ],
   },
   {
-    label: '上海总部',
+    label: '数据集',
     value: 2,
     children: [
       {
-        label: '市场部',
+        label: 'OIA-ODIR',
         value: '2-0',
       },
       {
-        label: '财务部',
+        label: 'kaggle',
         value: '2-1',
         children: [
           {
-            label: '财务1组',
+            label: '1.jpg',
             value: '2-1-0',
           },
           {
-            label: '财务2组',
+            label: '2.jpg',
             value: '2-1-1',
           },
         ],
       },
     ],
-  },
-  {
-    label: '湖南',
-    value: 3,
-  },
-  {
-    label: '湖北',
-    value: 4,
   },
 ];
