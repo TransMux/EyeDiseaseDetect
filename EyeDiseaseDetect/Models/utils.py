d109e835988bf09fc054d353506baaa80c286666
InEase <InEase28@gmail.com>
2022-04-11 21:15:08 +0800

    feat: 设计检测结果模板

--- EyeDiseaseDetect/Models/utils.py
+++ EyeDiseaseDetect/Models/utils.py
@@ -3,6 +3,44 @@ from pathlib import Path
 from typing import List
 
 
+def disease_detection_template(
+        name=None,
+        model=None,
+        status=None,
+        x1=None,
+        y1=None,
+        x2=None,
+        y2=None,
+        confidence=None
+):
+    return {
+        "name": name,
+        "model": model,
+        "status": status,
+        "x1": x1,
+        "y1": y1,
+        "x2": x2,
+        "y2": y2,
+        "confidence": confidence
+    }
+
+
+def risk_evaluate_template(
+        name=None,
+        model=None,
+        status=None,
+        disease=None,
+        confidence=None,
+):
+    return {
+        "name": name,
+        "model": model,
+        "status": status,
+        "disease": disease,
+        "confidence": confidence
+    }
+
+
 def change_status(data_path: List[Path], model_name: str, status: str) -> None:
     assert status in ["Waiting", "Predict", "Finish"]
 
